e9fd4cc9643182aad61c0171f972ec7068ad21f1
Vincent FAVRE-NICOLIN <favre@esrf.fr>
2021-12-16 09:14:48 +0100

    Give access to vkfft_version even if only the cuda backend is available

--- pyvkfft/version.py
+++ pyvkfft/version.py
@@ -12,7 +12,15 @@ def vkfft_version():
     Get VkFFT version
     :return: version as X.Y.Z
     """
-    # We import here as otherwise it would mess with setup.py which reads __vresion__
-    # while the opencl library as not yet been compiled.
-    from .opencl import vkfft_version as cl_vkfft_version
-    return cl_vkfft_version()
+    # We import here as otherwise it would mess with setup.py which reads __version__
+    # while the opencl library has not yet been compiled.
+    try:
+        from .opencl import vkfft_version as cl_vkfft_version
+        return cl_vkfft_version()
+    except ImportError:
+        # On some platforms (e.g. pp64le) opencl may not be available while cuda is
+        try:
+            from .cuda import vkfft_version as cu_vkfft_version
+            return cu_vkfft_version()
+        except ImportError:
+            raise ImportError("Neither cuda or opencl vkfft_version could be imported")
