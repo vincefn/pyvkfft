e310e165dec2cee7ce9b1c925b47cbc442a07338
Vincent Favre-Nicolin <favre@esrf.fr>
2024-02-17 00:16:49 +0100

    Vresion 2024.1.2

--- pyvkfft/version.py
+++ pyvkfft/version.py
@@ -2,9 +2,9 @@
 
 __authors__ = ["Vincent Favre-Nicolin (pyvkfft), Dmitrii Tolmachev (VkFFT)"]
 __license__ = "MIT"
-__date__ = "2024/02/12"
+__date__ = "2024/02/17"
 # Valid numbering includes 3.1, 3.1.0, 3.1.2, 3.1dev0, 3.1a0, 3.1b0, 3.1.2.post1,...
-__version__ = "2024.1.1"
+__version__ = "2024.1.2"
 
 import os
 
