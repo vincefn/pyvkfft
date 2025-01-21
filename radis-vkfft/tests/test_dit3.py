62c66651d6abe5772fc46a8f2f443ea0bd7d3878
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2025-01-21 22:47:38 +0100

    test_dit3 works
    fft bug solved by extending w axis by 1

--- radis-vkfft/tests/test_dit3.py
+++ radis-vkfft/tests/test_dit3.py
@@ -98,14 +98,15 @@ print('Done! {:.3f}'.format((tc1-tc0)*1e3))
 
 
 #%% CPU DIT method:
+
 def spectrum_dit(a):
-    k_arr = (database[1] - t_min) / dt
+    k_arr = (database[1] - np.float32(t_min)) / np.float32(dt)
     k0_arr = k_arr.astype(np.int32)
     k1_arr = k0_arr + 1
     a1k_arr = k_arr - k0_arr
     a0k_arr = 1 - a1k_arr
 
-    l_arr = (np.log(database[2]) - np.log(w_min)) / dxw
+    l_arr = (np.log(database[2]) - np.log(np.float32(w_min))) / np.float32(dxw)
     l0_arr = l_arr.astype(np.int32)
     l1_arr = l0_arr + 1
     a1l_arr = l_arr - l0_arr
@@ -129,7 +130,7 @@ def spectrum_dit(a):
     spectrum_FT = np.zeros(Nf, dtype=np.complex64)
     S_kl_FT = np.fft.rfft(S_kl)
     for l in range(Nw):
-        w_l = (1.0+a)*w_min*np.exp(l*dxw)
+        w_l = (1+a)*w_min*np.exp(l*dxw)
 
         #S_k_FT = np.fft.rfft(S_kl[l])
         S_k_FT = S_kl_FT[l]
@@ -142,7 +143,6 @@ def spectrum_dit(a):
         #I_arr1 += np.convolve(S_kl[l], ls, 'same')
 
     return np.fft.irfft(spectrum_FT)
-
 
 # lineshape distribution:
 print('Distributing... ', end='')
@@ -185,11 +185,12 @@ app.init_params_d.transferStagingBuffer(direction='H2D')
 
 app.iter_params_d.initStagingBuffer()
 iter_params_h = app.iter_params_d.getHostStructPtr(iter_params_t)
-
-app.S_kl_d.setFFTShape((Nw,Nt), np.float32)
-app.S_kl_FT_d.setFFTShape((Nw,Nf), np.complex64)
+app.S_kl_d.setFFTShape((Nw+1,Nt), np.float32)
+app.S_kl_FT_d.setFFTShape((Nw+1,Nf), np.complex64)
 app.spectrum_FT_d.setFFTShape(Nf, np.complex64)
 app.spectrum_d.setFFTShape(Nt, np.float32)
+
+#app.S_kl_FT_d.initStagingBuffer()
 
 app.spectrum_d.initStagingBuffer()
 
@@ -213,10 +214,18 @@ app.spectrum_d.toArray(I_arr2)
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
 
+#S_kl_FT2 = np.zeros_like(S_kl_FT)
+#app.S_kl_FT_d.copyFromBuffer(S_kl_FT2)
+
+
+
 
 #ax.plot(t_arr, I_arr0)
-p1, = ax.plot(t_arr, I_arr1-I_arr2,)
-##p2, = ax.plot(t_arr, I_arr2, 'k--')
+
+#p1 = ax.plot(f_arr, S_kl_FT.T.real)
+#p2 = ax.plot(f_arr, S_kl_FT2.T.real, 'k--')
+p1, = ax.plot(t_arr, I_arr1)
+p2, = ax.plot(t_arr, I_arr2, 'k--')
 
 axw = plt.axes([0.25, 0.1, 0.65, 0.03])
 sw = Slider(axw, "a", -1.0, 2.0, valinit=0.0)
@@ -233,8 +242,8 @@ def update(val):
     t2 = perf_counter()
 
     ax.set_title('CPU: {:.1f} ms - GPU: {:.1f} ms'.format((t1-t0)*1e3, (t2-t1)*1e3))
-    p1.set_ydata(I_arr1-I_arr2)
-##    p2.set_ydata(I_arr2)
+    p1.set_ydata(I_arr1)
+    p2.set_ydata(I_arr2)
     
     fig.canvas.draw_idle()
 
@@ -244,111 +253,3 @@ plt.show()
 
 
 
-
-### command buffer:
-##app.command_list = [
-##    # app.cmdAddTimestamp("start"),
-##    app.cmdClearBuffer(app.S_kl_d),
-##    # app.cmdFillLDM((init_h.N_lines // N_tpb + 1, 1, 1), threads, timestamp=True),
-##    # app.cmdClearBuffer(app.S_klm_FT_d, timestamp=True),
-##    app.cmdFFT(app.I_arr_d, app.I_arr_FT_d, timestamp=True),
-##    # app.cmdClearBuffer(app.spectrum_FT_d, timestamp=True),
-##    #app.cmdApplyTestLineshape(
-##    #    (Nf * Nb // Ntpb + 1, 1, 1), threads, timestamp=True
-##    #),
-##    # app.cmdClearBuffer(app.spectrum_d, timestamp=True),
-##    #app.cmdIFFT(app.I_arr_FT_d, app.res_d, timestamp=True),
-##]
-##app.writeCommandBuffer()
-
-
-
-
-
-
-
-
-#%% Plotting:
-# plt.plot(t_arr, I_arr0)
-# plt.plot(t_arr, I_arr1, 'k--')
-# plt.show()
-
-##I_arr_FT = np.fft.rfft(I_arr)
-##
-### Init Vulkan
-##shader_path = os.path.dirname(__file__)
-##app = GPUApplication(deviceID=1, path=shader_path)
-##
-##
-##app.params_d = GPUStruct.fromStruct(params_h, binding=0)
-##app.I_arr_d = GPUArray.fromArr(I_arr, binding=1)
-##app.I_arr_FT_d = GPUArray((Nb, Nf), np.complex64, binding=2)
-##app.res_FT_d = GPUArray((Nb, Nf), np.complex64, binding=3)
-##app.res_d = GPUArray((Nb, Nt), np.float32,   binding=4)
-##
-####app.I_arr_d.setData(I_arr)
-##
-##app.command_list = [
-##    app.cmdAddTimestamp("start"),
-##    # app.cmdClearBuffer(app.S_klm_d, timestamp=True),
-##    # app.cmdFillLDM((init_h.N_lines // N_tpb + 1, 1, 1), threads, timestamp=True),
-##    # app.cmdClearBuffer(app.S_klm_FT_d, timestamp=True),
-##    app.cmdFFT(app.I_arr_d, app.I_arr_FT_d, timestamp=True),
-##    # app.cmdClearBuffer(app.spectrum_FT_d, timestamp=True),
-##    #app.cmdApplyTestLineshape(
-##    #    (Nf * Nb // Ntpb + 1, 1, 1), threads, timestamp=True
-##    #),
-##    # app.cmdClearBuffer(app.spectrum_d, timestamp=True),
-##    #app.cmdIFFT(app.I_arr_FT_d, app.res_d, timestamp=True),
-##]
-##app.writeCommandBuffer()
-##
-##
-##
-##def update(val):
-##    wL = sw.val
-##
-##    # Recalc ref
-##    t0 = perf_counter()
-##    lineshape_FT = L_FT(f_arr, wL)
-##    I_arr_FT = np.fft.rfft(I_arr) #* lineshape_FT[np.newaxis,:]
-####    res_ref = np.fft.irfft(I_arr_FT)
-##    res_ref = I_arr_FT.real   
-##    
-##    t1 = perf_counter()
-##    
-##    # Recalc gpu
-##    params_h.wL = wL
-##    app.params_d.setData(params_h)
-##    app.run()
-####    res_gpu = np.copy(app.res_d.getData())
-##    res_gpu = np.copy(app.I_arr_FT_d.getData().real)
-##    t2 = perf_counter()
-##
-####    print('ref:',(t1-t0)*1e3)
-####    print('gpu:',(t2-t1)*1e3)
-##    
-##    for i in range(Nb):
-##        lines_ref[i].set_ydata(res_ref[i] - res_gpu[i])
-####        lines_gpu[i].set_ydata(res_gpu[i])
-##    fig.canvas.draw_idle()
-##
-##
-##
-### Set up plotting
-##fig, ax = plt.subplots()
-##plt.subplots_adjust(left=0.25, bottom=0.25)
-##
-##ax.axhline(0, c="k", lw=1, alpha=0.5)
-##lines_ref = ax.plot(f_arr, np.zeros_like(I_arr_FT).real.T, '.', lw=1)
-##lines_gpu = ax.plot(f_arr, np.zeros_like(I_arr_FT).real.T, 'k--', lw=1)
-##
-##axw = plt.axes([0.25, 0.1, 0.65, 0.03])
-##sw = Slider(axw, "Width", 0.0, 2.0, valinit=w0)
-##update(w0)
-##ax.relim()
-##ax.set_xlim(-0.05,0.2)
-##sw.on_changed(update)
-##plt.show()
-##
-##
